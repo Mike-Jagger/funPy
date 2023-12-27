53634e85e25b9deb74907a5d22d59cdec963b2bd
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2023-12-26 19:35:43 -0700

    Added default values to arguments of the generate_password function

--- password_generator/main.py
+++ password_generator/main.py
@@ -3,7 +3,7 @@ import secrets
 import string
 
 
-def generate_password(length, nums, special_chars, uppercase, lowercase):
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase=1):
    
     # Define the possible characters for the password
     letters = string.ascii_letters
@@ -35,5 +35,5 @@ def generate_password(length, nums, special_chars, uppercase, lowercase):
     
     return password
     
-new_password = generate_password(8, 1, 1, 1, 1)
+new_password = generate_password(length = 8)
 print(new_password)
