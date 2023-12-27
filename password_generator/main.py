c7e2694450dc0c42effd0cf0f7b692ca86543460
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2023-12-26 19:39:23 -0700

    removed all function arguments in function call to use default arguments and improved the print statement

--- password_generator/main.py
+++ password_generator/main.py
@@ -35,5 +35,5 @@ def generate_password(length=16, nums=1, special_chars=1, uppercase=1, lowercase
     
     return password
     
-new_password = generate_password(length = 8)
-print(new_password)
+new_password = generate_password()
+print("Generated password:", new_password)
