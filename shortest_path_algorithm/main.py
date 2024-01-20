16afe7b411c20bf3b607d2491a5091095b9f2eca
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-01-19 18:39:02 -0700

    Initializing the graph my_graph

--- shortest_path_algorithm/main.py
+++ shortest_path_algorithm/main.py
@@ -1,9 +1,6 @@
-copper = {
-    'species': 'guinea pig',
-    'age': 2
+my_graph = {
+    'A': [('B', 3), ('D', 1)],
+    'B': [('A', 3), ('C', 4)],
+    'C': [('B', 4), ('D', 7)],
+    'D': [('A', 1), ('C', 7)]
 }
-
-copper['food'] = 'hay'
-copper['species'] = 'Cavia porcellus'
-del copper['age']
-print(copper)
