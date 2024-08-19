8a290eabc5470bacd39e5825d4b48e7854257688
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-18 23:28:49 -0600

    Adding interface for a stack class

---
+++ Algorithms and Datastructures/dataStructures/stack.js
@@ -0,0 +1,14 @@
+// Stack class
+class Stack {
+	// Array is used to implement stack
+	constructor() {
+		this.items = [];
+	}
+
+	// Functions to be implemented
+	// push(item)
+	// pop()
+	// peek()
+	// isEmpty()
+	// printStack()
+}
