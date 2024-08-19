46b64100f396d6a0e380cb8a8ecc8025caef2371
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-18 23:30:28 -0600

    implementing pop function

--- Algorithms and Datastructures/dataStructures/stack.js
+++ Algorithms and Datastructures/dataStructures/stack.js
@@ -11,7 +11,14 @@ class Stack {
 		this.items.push(element);
 	}
 
-	// pop()
+	// pop function
+	pop() {
+		// return top most element in the stack
+		// and removes it from the stack
+		// Underflow if stack is empty
+		if (this.items.length == 0) return "Underflow";
+		return this.items.pop();
+	}
 	// peek()
 	// isEmpty()
 	// printStack()
